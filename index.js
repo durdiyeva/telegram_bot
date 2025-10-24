1ca63ff9f6e2f41a4b74d70abc99927070471320
zarin <durdiyevazarina1@gmail.com>
2025-10-24 17:20:10 +0500

    tg bot

---
+++ index.js
@@ -0,0 +1,10 @@
+import TelegramBot from "node-telegram-bot-api";
+
+const TOKEN = "7535599772:AAEKlCk2hLCwIOdUTRtZ5L7Xs9zCOme_J4c"
+
+const bot = new TelegramBot (TOKEN, {polling: true})
+
+bot.on("message", function(msg){
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId,"Salom")
+})
