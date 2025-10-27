74c6ddefae73dc4a97f7906cc7dd62c512c23b59
zarin <durdiyevazarina1@gmail.com>
2025-10-27 18:17:11 +0500

    ozgarish saqlandi

--- index.js
+++ index.js
@@ -2,11 +2,22 @@ import TelegramBot from "node-telegram-bot-api";
 
 const TOKEN = "7535599772:AAEKlCk2hLCwIOdUTRtZ5L7Xs9zCOme_J4c"
 
-const bot = new TelegramBot (TOKEN, {polling: true})
+const bot = new TelegramBot(TOKEN, { polling: true })
 
-bot.on("message", function(msg){
+bot.on("message", function (msg) {
     const chatId = msg.chat.id;
-    bot.sendMessage(chatId,"Salom")
+    const firstname = msg.chat.first_name;
+    if (text == "/start") {
+        bot.sendMessage(chatId, `xush kelibsiz, ${firstname}`, {
+            reply_markup: {
+                keyboard: [
+                    [{ text: "boshlash" }],
+                    [{ text: "menu" }, { text: "sozlamalar" }],
+                ],
+                resize_keyboard: true,
+            },
+        });
+    } else if (text == "boshlash")
 })
 
 console.log("bot ishga tushdi")
