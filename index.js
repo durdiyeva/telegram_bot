58632c9dd585c7c1e2ddb74c206bd52fad5c55b2
zarin <durdiyevazarina1@gmail.com>
2025-11-03 17:54:02 +0500

    telegram botga yangi funksiyalar qoshildi

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const TOKEN = "7535599772:AAEKlCk2hLCwIOdUTRtZ5L7Xs9zCOme_J4c";
 
 const bot = new TelegramBot(TOKEN, { polling: true });
 
-bot.on("message", function (msg) {
+let lambophotoURL = "./imeges/rasm.jpg";
+
+bot.on("message", async function (msg) {
   const chatId = msg.chat.id;
   const text = msg.text;
   const firstname = msg.chat.first_name;
@@ -19,7 +21,27 @@ bot.on("message", function (msg) {
       },
     });
   } else if (text == "Boshlash 🔥") {
-    bot.sendMessage(chatId, "Boshlanyaptii....");
+   const xabar = await bot.sendMessage(chatId, "Iltimos kuting...");
+   setTimeout(function () {
+    bot.deleteMessage(chatId, xabar.message_id);
+
+    bot.sendPhoto(chatId, lambophotoURL, {
+      caption:`
+🔹 Ishlab chiqaruvchi: Lamborghini (Italiya)
+🔹 Model: Centenario LP770-4
+🔹 Chiqarilgan yili: 2016-yil (cheklangan seriya)
+🔹 Dvigatel: 6.5 litrli V12, atmosferali
+🔹 Quvvat: 770 ot kuchi
+🔹 Uzatuv tizimi: 7 pog'onali ISR (dual-clutch) avtomatik, to‘liq yuritma (AWD)
+🔹 Tezlanish (0–100 km/soat): 2.8 soniya
+🔹 Maksimal tezlik: 350 km/soatdan ortiq
+🔹 Ishlab chiqarilgan soni: atigi 40 dona (20 kupé, 20 roadster)
+
+Qiziqarli fakt:
+Centenario modeli Lamborghini asoschisi Ferruccio Lamborghini’nin 100 yilligiga bag‘ishlab yaratilgan. U Aventador platformasiga asoslangan, lekin aerodinamikasi va dizayni yanada ilg‘or.
+`,
+    });
+   }, 1000)
   } else if (text == "Menu 🥩") {
     bot.sendMessage(chatId, "Menyuga xush kelibsiz....");
   } else if (text == "Sozlamalar ⚙️") {
